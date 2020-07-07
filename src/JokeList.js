d6b40008e08ecde9a0f5fc17958e1539f1c45736
unknown <exgin3538@yahoo.com>
2020-07-07 17:44:42 -0400

    refactor Joke & work on JokeList

--- src/JokeList.js
+++ src/JokeList.js
@@ -5,14 +5,81 @@ import './JokeList.css';
 
 // props = numJokesToGet
 class JokeList extends React.Component {
-  static defaultProps = {numJokesToGet = 10}
+  // static defaultProps = {numJokesToGet = 10};
 
   constructor(props) {
-    super(props)
-    this.state = { jokes };
+    super(props);
+    this.state = { jokes: [] };
+    // this.generateNewJokes = this.generateNewJokes(this);
+    this.resetVotes = this.resetVotes.bind(this);
+    this.vote = this.vote.bind(this);
   }
 
+  componentDidMount() {
+    // mount our jokes
+  }
 
+  // get tge jokes from jokes API
+  async getJokes() {
+    try {
+      let jokes = this.state.jokes;
+
+      // find all of the votes within localStorage
+      let jokeVotes = JSON.parse(window.localStorage.getItem('jokeVotes') || '{}');
+
+      // remove all the same jokes from the API that we get
+      let seenJokes = new Set(jokes.map((joke) => joke.id));
+
+      // get jokes all the way up to 10
+      while (jokes.length < this.props.numJokesToGet) {
+        // fetch data
+        let results = await axios.get('https://icanhazdadjoke.com', { headers: { Accept: 'application/json' } });
+        let { status, ...jokeObj } = results.data;
+
+        if (!seenJokes.has(jokeObj.id)) {
+          seenJokes.add(jokeObj.id);
+          jokeVotes.push({ ...jokeObj, votes: 0 });
+        } else {
+          console.log('duplicate joke found!');
+        }
+      }
+
+      this.setState({ jokes });
+      window.localStorage.setItem('jokeVotes', JSON.stringify(jokeVotes));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  vote(id, num) {
+    let jokeVotes = JSON.parse(window.localStorage.getItem('jokeVotes')); // get all the jokes from localStorage
+    jokeVotes[id] = (jokeVotes[id] || 0) + num;
+    window.localStorage.setItem('jokeVotes', JSON.stringify(jokeVotes));
+    this.setState((state) => ({
+      jokes: state.jokes.map((j) => (j.id === id ? { ...j, votes: j.votes + num } : j)),
+    }));
+  }
+
+  // generateNewJokes() {
+  //   this.setState((state) => ({jokes: state.jokes.filter((joke) => joke)}))
+  // }
+
+  resetVotes() {
+    window.localStorage.setItem('jokeVotes', '{}');
+    this.setState((st) => ({
+      jokes: st.jokes.map((joke) => ({ ...joke, votes: 0 })),
+    }));
+  }
+
+  render() {
+    const { numJokesToGet } = this.props;
+
+    return (
+      <div className='JokeList'>
+        <button className='JokeList-getmore'>Get New Jokes</button>
+      </div>
+    );
+  }
 }
 
 export default JokeList;
